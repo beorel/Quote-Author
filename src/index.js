81785add98e9ba09ba2eb4dfb95675e36bbda711
Popoola Beauty <beautypopoola14@gmail.com>
2021-11-03 12:28:55 +0100

    feature: Quote with wrapper component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import QuoteWrapper from './quoteWrapper';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QuoteWrapper />
   </React.StrictMode>,
   document.getElementById('root')
 );
